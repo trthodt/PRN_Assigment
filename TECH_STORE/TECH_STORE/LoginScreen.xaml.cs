f1fb8eeccf9edf32fe199244161a973f829e2ceb
ThoNT16 <truongthonguyen107@gmail.com>
2024-10-28 21:11:41 +0700

    feat: init project

---
+++ TECH_STORE/TECH_STORE/LoginScreen.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using Tech_BussinessObjects;
+using Tech_Repositories.Interface;
+using Tech_Services.Implement;
+using Tech_Services.Interface;
+
+namespace TECH_STORE
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        private IUserService _userService;
+        public LoginScreen()
+        {
+            InitializeComponent();
+            _userService = new UserService();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = pbPassword.Password;
+            if (string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter Email and Password");
+                return;
+            }
+            User user = _userService.FindUserByEmail(email);
+            if (user == null) {
+                MessageBox.Show("Wrong Email");
+                return;
+            }
+            if (user.Password.Equals(password)) {
+                ProductScreen screen = new ProductScreen();
+                screen.Show();
+            } else
+            {
+                MessageBox.Show("Wrong password");
+            }
+        }
+    }
+}
---
+++ TECH_STORE/TECH_STORE/ProductList.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tech_Services.Implement;
+using Tech_Services.Interface;
+
+namespace TECH_STORE
+{
+    /// <summary>
+    /// Interaction logic for ProductList.xaml
+    /// </summary>
+    public partial class ProductList : Page
+    {
+        private IProductService _productService;
+        public ProductList()
+        {
+            InitializeComponent();
+            _productService = new ProductService();
+        }
+
+        private void LoadProduct()
+        {
+            dtgProductList.ItemsSource = _productService.GetProducts();
+        }
+
+        private void pgProductList_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadProduct();
+        }
+    }
+}
